0dc6035fb11dbce957b6c8ab26eef3c7afe53b06
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2022-02-02 22:12:41 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+module.exports = function towelSort(matrix) {
+    const array = [];
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix == null) return []
+    // пройдемся по матрице по змеиному
+    for (let i = 0; i < matrix.length; i++) {
+        for (j = 0; j < matrix[i].length; j++) {
+            let index = i % 2 === 0 ? j : (matrix[i].length - 1 - j)
+            array.push(matrix[i][index])
+        }
+    }
+    return array;
 }
